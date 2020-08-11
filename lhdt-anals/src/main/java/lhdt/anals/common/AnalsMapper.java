13405331e772488da8f1a00dd0392ce3d0f34e69
HyunSeongKil <aha1492@gmail.com>
2020-08-11 13:14:48 +0900

    init. mybatis  mapper의 부모 클래스

---
+++ lhdt-anals/src/main/java/lhdt/anals/common/AnalsMapper.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package lhdt.anals.common;
+
+/**
+ * @author gravity@daumsoft.com
+ * @since 2020. 8. 10.
+ *
+ */
+public interface AnalsMapper<DOMAIN, IDTYPE> {
+
+	Integer getTotcnt();
+}
