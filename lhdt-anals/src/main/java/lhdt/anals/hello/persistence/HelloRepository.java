b378baf5f7f01e5c7e201fa690a8180546f84b80
HyunSeongKil <aha1492@gmail.com>
2020-08-11 13:20:55 +0900

    refactoring

--- lhdt-anals/src/main/java/lhdt/anals/hello/persistence/HelloRepository.java
+++ lhdt-anals/src/main/java/lhdt/anals/hello/persistence/HelloRepository.java
@@ -14,4 +14,19 @@ import lhdt.anals.hello.domain.Hello;
  */
 public interface HelloRepository extends JpaRepository<Hello, Long> {
 
+	/**
+	 * 업무키로 데이터 존재여부 확인
+	 * @param helloGroupId
+	 * @param helloGroupNo
+	 * @return
+	 */
+	boolean existsByHelloGroupIdAndHelloGroupNo(String helloGroupId, Long helloGroupNo);
+	
+	/**
+	 * 업무키로 데이터 1건 조회
+	 * @param helloGroupId
+	 * @param helloGroupNo
+	 * @return
+	 */
+	Hello findByHelloGroupIdAndHelloGroupNo(String helloGroupId, Long helloGroupNo);
 }
