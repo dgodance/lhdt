af7cc3efb9e88b4ec642dca8e8ca7105da41dfa7
HyunSeongKil <aha1492@gmail.com>
2020-08-18 13:24:39 +0900

    add saveCaptureAndSkylineImages()

--- lhdt-svc/src/main/java/lhdt/svc/landscape/controller/LandScapeAnalsController.java
+++ lhdt-svc/src/main/java/lhdt/svc/landscape/controller/LandScapeAnalsController.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import lhdt.svc.landscape.model.LandScapeAnalsParam;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -23,14 +22,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.geo.Point;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lhdt.svc.common.SvcController;
+import lhdt.svc.common.SvcUtils;
 import lhdt.svc.landscape.domain.LandScapeAnals;
+import lhdt.svc.landscape.model.LandScapeAnalsParam;
 import lhdt.svc.landscape.service.LandScapeAnalsService;
 import lhdt.svc.landscape.types.LandScapeAnalsType;
 import lombok.extern.slf4j.Slf4j;
@@ -267,6 +275,85 @@ public class LandScapeAnalsController extends SvcController {
 
 		//
 		return new ResponseEntity<Map<String,Object>>(resultMap, HttpStatus.OK);
+	}
+	
+	
+	/**
+	 * 캡처 이미지, 스카이라인 이미지 파일로 저장
+	 * TODO 테이블 설계 필요. 이미지 정보 테이블에 저장해야 함
+	 * @param request
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
+	@CrossOrigin(origins = "*")
+	@PostMapping(value="/saveCaptureAndSkylineImages")
+	@ResponseBody
+	public ResponseEntity<Map<String,Object>> saveCaptureAndSkylineImages(MultipartHttpServletRequest request) throws IllegalStateException, IOException{
+		Map<String,MultipartFile> fileMap = request.getFileMap();
+		
+		//
+		String groupId = registGroupDummy("dummyUserId");
+		
+		//
+		for(int i=0; i<3; i++) {
+			//
+			MultipartFile captureFile = fileMap.get("capture["+i+"]");
+			String captureFilename = groupId + "_" + i + SvcUtils.createShortUid("_") + ".png";
+			saveToFile(captureFile, captureFilename);
+			registDummy(groupId, "capture", i, captureFilename);
+
+			//
+			MultipartFile skylineFile = fileMap.get("skyline["+i+"]");
+			String skylineFilename = groupId + "_" + i + SvcUtils.createShortUid("_") + ".png";
+			saveToFile(skylineFile, skylineFilename);
+			registDummy(groupId, "skyline", i, skylineFilename);
+		}
+
+		//
+		return null;
+	}
+	
+	/**
+	 * 데이터(이미지 정보) 등록. 임시
+	 * @param groupId
+	 * @param imageGbn
+	 * @param gbn
+	 * @param filename
+	 */
+	private void registDummy(String groupId, String imageGbn, int gbn, String filename) {
+		log.debug("{} {} {} {}", groupId, imageGbn, gbn, filename);
+	}
+
+	/**
+	 * 그룹에 데이터 등록. 임시
+	 * @param userId
+	 * @return
+	 */
+	private String registGroupDummy(String userId) {
+		String groupId = SvcUtils.createShortUid("G");
+		
+		//
+		log.debug(".registGroup - {} {}", userId, new Date());
+		
+		//
+		return groupId;
+	}
+	
+	
+	/**
+	 * 파일 저장
+	 * @param mfile
+	 * @param filename
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
+	private String saveToFile(MultipartFile mfile, String filename) throws IllegalStateException, IOException {
+		mfile.transferTo(Paths.get(fileUploadPath, filename));
+		
+		//
+		return filename;
 	}
 
 }
