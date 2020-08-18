bc803f5b95fee029ffb29ff01ed445021e41a57a
shpark <shpark@gaia3d.com>
2020-08-18 18:47:42 +0900

    정리

---
+++ lhdt-user/src/main/java/lhdt/api/DesignLayerGroupController.java
@@ -0,0 +1,10 @@
+package lhdt.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/design-layer-groups")
+public class DesignLayerGroupController {
+
+}
---
+++ lhdt-user/src/test/java/lhdt/api/DesignLayerGroupControllerTests.java
@@ -0,0 +1,7 @@
+package lhdt.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DesignLayerGroupControllerTests {
+
+}
