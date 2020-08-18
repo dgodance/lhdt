00e832315a23e93747b8e04a42047ac8164ab3fc
kimdohyeon <ckdic8524@naver.com>
2020-08-18 16:38:18 +0900

    board view update

--- lhdt-svc/src/main/java/lhdt/svc/config/WebMvcConfig.java
+++ lhdt-svc/src/main/java/lhdt/svc/config/WebMvcConfig.java
@@ -6,7 +6,10 @@ package lhdt.svc.config;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +17,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lhdt.svc.common.interceptor.MiscInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 /**
  * @author gravity@daumsoft.com
@@ -24,6 +35,10 @@ import lombok.extern.slf4j.Slf4j;
 public class WebMvcConfig implements WebMvcConfigurer {
 	@Autowired
 	private MiscInterceptor miscInterceptor;
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	
 	@PostConstruct
 	private void init() {
