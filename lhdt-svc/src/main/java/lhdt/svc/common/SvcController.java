17d25bab8dfe454499645532874d8f66dc853147
HyunSeongKil <aha1492@gmail.com>
2020-08-19 17:04:36 +0900

    add getUserSession(), getUserId(), getUserName()

--- lhdt-svc/src/main/java/lhdt/svc/common/SvcController.java
+++ lhdt-svc/src/main/java/lhdt/svc/common/SvcController.java
@@ -3,13 +3,63 @@
  */
 package lhdt.svc.common;
 
+
+import javax.servlet.http.HttpServletRequest;
+
+import lhdt.ds.common.misc.DsConst;
 import lhdt.ds.common.misc.DsController;
+import lhdt.svc.lhdt.domain.UserSession;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 모든 controller  의 부모
  * @author gravity
  *
  */
+@Slf4j
 public class SvcController extends DsController{
+	
+	
+	protected UserSession getUserSession(HttpServletRequest request) {
+		Object obj = request.getSession().getAttribute(DsConst.USER_SESSION);
+		if(null == obj) {
+			log.warn("<<.getUserSession - null userSession");
+			return null;
+		}
+		
+		//
+		return (UserSession)obj;
+	}
 
+	/**
+	 * 세션에 저장된 사용자 정보에서 userId 추출
+	 * @param request
+	 * @return
+	 */
+	protected String getUserId(HttpServletRequest request) {
+		Object obj = getUserSession(request);
+		if(null == obj) {
+			log.warn("<<.getUserId - null userSession");
+			return null;
+		}
+		
+		//
+		return ((UserSession)obj).getUserId();
+	}
+	
+	/**
+	 * 세션에 저장된 사용자 정보에서 userName 추출
+	 * @param request
+	 * @return
+	 */
+	protected String getUserName(HttpServletRequest request) {
+		Object obj = getUserSession(request);
+		if(null == obj) {
+			log.warn("<<.getUserName - null userSession");
+			return null;
+		}
+		
+		//
+		return ((UserSession)obj).getUserName();
+	}
 }
