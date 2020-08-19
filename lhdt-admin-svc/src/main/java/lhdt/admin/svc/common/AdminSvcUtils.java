d867fe7b6dee6be9817efafab21f819a978f8100
HyunSeongKil <aha1492@gmail.com>
2020-08-19 16:32:14 +0900

    add getUserSession()

--- lhdt-admin-svc/src/main/java/lhdt/admin/svc/common/AdminSvcUtils.java
+++ lhdt-admin-svc/src/main/java/lhdt/admin/svc/common/AdminSvcUtils.java
@@ -3,7 +3,13 @@
  */
 package lhdt.admin.svc.common;
 
+import javax.servlet.http.HttpServletRequest;
+
+import dev.hyunlab.web.util.PpWebSessionUtil;
+import lhdt.admin.svc.lhdt.domain.UserSession;
+import lhdt.ds.common.misc.DsConst;
 import lhdt.ds.common.misc.DsUtils;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 유틸리티
@@ -11,6 +17,25 @@ import lhdt.ds.common.misc.DsUtils;
  * @since 2020. 8. 18.
  *
  */
+@Slf4j
 public class AdminSvcUtils extends DsUtils {
+	
+	
+	/**
+	 * 세션에서 usersession정보 추출
+	 * @param request
+	 * @return
+	 */
+	public static UserSession getUserSession(HttpServletRequest request) {
+		Object obj = PpWebSessionUtil.get(request, DsConst.USER_SESSION);
+		
+		if(null == obj) {
+			log.warn("<<.getUserSession - null userSession");
+			return null;
+		}
+		
+		//
+		return (UserSession)obj;
+	}
 
 }
