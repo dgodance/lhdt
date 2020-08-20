a3bd3f08443a331aac2f8fc3fcbed51c09d9ae6b
HyunSeongKil <aha1492@gmail.com>
2020-08-20 19:52:41 +0900

    fix

--- lhdt-admin-svc/src/main/java/lhdt/admin/svc/lowinfo/service/impl/LowInfoDetServiceImpl.java
+++ lhdt-admin-svc/src/main/java/lhdt/admin/svc/lowinfo/service/impl/LowInfoDetServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-@Service
+@Service("lowInfoDetService")
 public class LowInfoDetServiceImpl
         extends AdminSvcServiceImpl<LowInfoDetRepository, LowInfoDetMapper, LowInfoDet, Long> implements LowInfoDetService {
     @Autowired
