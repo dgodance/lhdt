10e2ef3d2c4bf3c0d759266926533e249fb81718
HyunSeongKil <aha1492@gmail.com>
2020-08-25 17:03:45 +0900

    fix metronic

---
+++ lhdt-admin-svc/src/main/java/lhdt/admin/svc/IndexController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package lhdt.admin.svc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lhdt.admin.svc.common.AdminSvcController;
+
+/**
+ * @author gravity@daumsoft.com
+ * @since 2020. 8. 20.
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController extends AdminSvcController {
+
+	@GetMapping("index")
+	public String index() {
+		return "index";
+	}
+}
