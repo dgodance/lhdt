0b1bd94758b16fccac2948aea5d268a95d4f3406
yhjeong1009 <yhjeong@gaia3d.com>
2020-09-01 17:14:58 +0900

    지역이동 코드 리펙토링 지역이동 템플릿화

--- lhdt-user/src/main/resources/static/js/ko/district-controll.js
+++ lhdt-user/src/main/resources/static/js/ko/district-controll.js
@@ -7,19 +7,28 @@ function DistrictControll(magoInstance) {
 }
 
 function District(magoInstance, viewer) {
+
     this.drawDistrict = function (name, sdoCode, sggCode, emdCode, bjcdLen) {
+
         this.deleteDistrict();
         var now = new Date();
         var rand = (now - now % 5000) / 5000;
         var policy = NDTP.policy;
+
+        var sdoCodeStr = sdoCode.toString();
+        var sggCodeStr = sggCode.toString();
+        var emdCodeStr = emdCode.toString();
+
         // 시도(2) + 시군구(3) + 읍면동(3) + 리(2)
         var queryString;
-        if (bjcdLen==10){
-        	queryString = "bjcd = " + sdoCode.toString().padStart(2, '0') + sggCode.toString().padStart(3, '0') + emdCode.toString().padStart(3, '0') + '00';
-        }else{
-        	queryString = "bjcd = " + sdoCode.toString() + sggCode.toString() + emdCode.toString();
+        if (bjcdLen == 10) {
+            queryString = "bjcd = " + sdoCodeStr.padStart(2, '0')
+                                    + sggCodeStr.padStart(3, '0')
+                                    + emdCodeStr.padStart(3, '0') + '00';
+        } else {
+            queryString = "bjcd = " + sdoCodeStr + sggCodeStr + emdCodeStr;
         }
-        console.log(sdoCode.toString().length);
+
         // TODO 개발 서버 포팅후 geoserver url 변경하기 
         var provider = new Cesium.WebMapServiceImageryProvider({
             url: policy.geoserverDataUrl + "/wms",
@@ -63,40 +72,6 @@ var emdCode = "";
 var bjcdLen;
 var districtMapType = 1;
 
-var defaultDistrictSdoObject = '<li class="on" onclick="changeSdo(this, \'\')">전체</li>';
-//var defaultDistrictSggObject = '<li class="on" onclick="changeSgg(this, sdoCode, \'\')">전체</li>';
-//var defaultDistrictEmdObject = '<li class="on" onclick="changeEmd(this, sdoCode, sggCode, \'\')">전체</li>';
-
-function updateViewDistrictName() {
-    // 시군구가 blank
-    if (sggName.trim() === "" || sdoName === sggName) {
-        // 읍면동이 blan 이거나 시도랑 같은 경우
-        if (emdName.trim() === "" || sdoName === emdName) {
-            $("#viewDistrictName").html([sdoName]);
-        } else {
-            $("#viewDistrictName").html([sdoName, emdName].join(" "));
-        }
-    } else {
-        // 시도랑 시군구랑 다를때
-        // 시도랑 읍면동이 같을때
-        if (sdoName === emdName) {
-            $("#viewDistrictName").html([sdoName, sggName].join(" "));
-        } else {
-            if (sggName === emdName) {
-                $("#viewDistrictName").html([sdoName, sggName].join(" "));
-            } else {
-                $("#viewDistrictName").html([sdoName, sggName, emdName].join(" "));
-            }
-        }
-
-        if (sggName === emdName) {
-            $("#viewDistrictName").html([sdoName, sggName].join(" "));
-        } else {
-            $("#viewDistrictName").html([sdoName, sggName, emdName].join(" "));
-        }
-    }
-}
-
 /**
  * 시도 목록을 로딩
  */
@@ -108,16 +83,12 @@ function loadDistrict() {
         dataType: "json",
         success: function (msg) {
             if (msg.statusCode <= 200) {
-                var sdoList = msg.sdoList;
-                var content = "";
-                bjcdLen = sdoList[0].bjcd.length;
 
-                content += defaultDistrictSdoObject;
-                for (var i = 0, len = sdoList.length; i < len; i++) {
-                	var sdo = sdoList[i];
-                    content += '<li onclick="changeSdo(this, ' + sdo.sdoCode + ')">' + sdo.koname + '<span></span></li>';
-                }
-                $('#sdoList').html(content);
+                bjcdLen = msg.sdoList[0].bjcd.length;
+
+                // 핸들바 템플릿 컴파일
+                updateSelectSdo(msg);
+
             } else {
                 alert(JS_MESSAGE[msg.errorCode]);
                 console.log("---- " + msg.message);
@@ -129,27 +100,37 @@ function loadDistrict() {
     });
 }
 
-// 시도가 변경되면 하위 시군구, 읍면동이 변경됨
+/**
+ * 시도가 변경되면 하위 시군구, 읍면동이 변경됨
+ * @param _this
+ * @param _sdoCode
+ * @returns {boolean}
+ */
 function changeSdo(_this, _sdoCode) {
     sdoCode = _sdoCode;
     sggCode = "";
     emdCode = "";
     districtMapType = 1;
 
-    var defaultDistrictSggObject = '<li class="on" onclick="changeSgg(this, ' + sdoCode + ', ' + sggCode + ')">전체</li>';
-    var defaultDistrictEmdObject = '<li class="on" onclick="changeEmd(this, ' + sdoCode + ', ' + sggCode + ', ' + emdCode + ')">전체</li>';
-
     if (!sdoCode) {
         sdoCode = "";
-        $('#sggList').html(defaultDistrictSggObject);
-        $('#emdList').html(defaultDistrictEmdObject);
-        $("#sdoList li").removeClass("on");
-        $(_this).addClass('on');
-        $('#districtNav span').removeClass("on");
-        $('#sdoName').addClass("on")
-        $('#sdoName').html("시도");
-        $('#sggName').html("시군구");
-        $('#emdName').html("읍면동");
+
+        var msg = {
+            sdoCode : sdoCode,
+            sggCode : sggCode,
+            emdCode : emdCode,
+            isSelectedSdo : true,
+            sdoName : '',
+            sggName : '',
+            emdName : ''
+        };
+
+        // 핸들바 템플릿 컴파일
+        updateSelectSgg(msg);
+        updateSelectEmd(msg);
+        updateSelectNav(msg);
+        updateSelectedElement("#districtSelectSdoDHTML li", _this);
+
         return false;
     }
 
@@ -160,31 +141,24 @@ function changeSdo(_this, _sdoCode) {
         dataType: "json",
         success: function (msg) {
             if (msg.statusCode <= 200) {
-                var sggList = msg.sggList;
-                var content = "";
-
-                content += defaultDistrictSggObject;
-                for (var i = 0, len = sggList.length; i < len; i++) {
-                    var sgg = sggList[i];
-                    content += '<li onclick="changeSgg(this, ' + sdoCode + ', ' + sgg.sggCode + ')">' + sgg.koname + '</li>';
-                }
                 sdoName = $(_this).text();
                 sggName = "";
                 emdName = "";
 
-                $('#districtNav span').removeClass("on");
-                $('#sdoName').addClass("on")
-                $('#sdoName').html(sdoName ? sdoName : "시도");
-                $('#sggName').html(sggName ? sggName : "시군구");
-                $('#emdName').html(emdName ? emdName : "읍면동");
+                msg.sdoCode = sdoCode;
+                msg.sggCode = sggCode;
+                msg.emdCode = emdCode;
+                msg.isSelectedSdo = true;
+                msg.sdoName = sdoName;
+                msg.sggName = sggName;
+                msg.emdName = emdName;
 
-                $('#sggList').html(content);
-                $('#emdList').html(defaultDistrictEmdObject);
+                // 핸들바 템플릿 컴파일
+                updateSelectSgg(msg);
+                updateSelectEmd(msg);
+                updateSelectNav(msg);
+                updateSelectedElement("#districtSelectSdoDHTML li", _this);
 
-                $("#sdoList li").removeClass("on");
-                $(_this).addClass('on');
-
-                updateViewDistrictName();
             } else {
                 alert(JS_MESSAGE[msg.errorCode]);
                 console.log("---- " + msg.message);
@@ -196,25 +170,38 @@ function changeSdo(_this, _sdoCode) {
     });
 }
 
-// 시군구가 변경되면 하위 읍면동이 변경됨
+/**
+ * 시군구가 변경되면 하위 읍면동이 변경됨
+ * @param _this
+ * @param _sdoCode
+ * @param _sggCode
+ * @returns {boolean}
+ */
 function changeSgg(_this, _sdoCode, _sggCode) {
     sdoCode = _sdoCode;
     sggCode = _sggCode;
     emdCode = "";
     districtMapType = 2;
-    var defaultDistrictEmdObject = '<li class="on" onclick="changeEmd(this, ' + sdoCode + ', ' + sggCode + ', ' + emdCode + ')">전체</li>';
 
     if (!sggCode) {
         sggCode = "";
         districtMapType = 1;
-        $('#emdList').html(defaultDistrictEmdObject);
-        $("#sggList li").removeClass("on");
-        $(_this).addClass('on');
-        $('#districtNav span').removeClass("on");
-        $('#sdoName').addClass("on")
-        $('#sdoName').html(sdoName);
-        $('#sggName').html("시군구");
-        $('#emdName').html("읍면동");
+
+        var msg = {
+            sdoCode : sdoCode,
+            sggCode : sggCode,
+            emdCode : emdCode,
+            isSelectedSdo : true,
+            sdoName : sdoName,
+            sggName : "",
+            emdName : ""
+        };
+
+        // 핸들바 템플릿 컴파일
+        updateSelectEmd(msg);
+        updateSelectNav(msg);
+        updateSelectedElement("#districtSelectSggDHTML li", _this);
+
         return false;
     }
 
@@ -225,29 +212,22 @@ function changeSgg(_this, _sdoCode, _sggCode) {
         dataType: "json",
         success: function (msg) {
             if (msg.statusCode <= 200) {
-                var emdList = msg.emdList;
-                var content = "";
-
-                content += defaultDistrictEmdObject;
-                for (var i = 0, len = emdList.length; i < len; i++) {
-                    var emd = emdList[i];
-                    content += '<li onclick="changeEmd(this, ' + sdoCode + ', ' + sggCode + ', ' + emd.emdCode + ')">' + emd.koname + '</li>';
-                }
                 sggName = $(_this).text();
                 emdName = "";
 
-                $('#districtNav span').removeClass("on");
-                $('#sggName').addClass("on")
-                $('#sdoName').html(sdoName ? sdoName : "시도");
-                $('#sggName').html(sggName ? sggName : "시군구");
-                $('#emdName').html(emdName ? emdName : "읍면동");
+                msg.sdoCode = sdoCode;
+                msg.sggCode = sggCode;
+                msg.emdCode = emdCode;
+                msg.isSelectedSgg = true;
+                msg.sdoName = sdoName;
+                msg.sggName = sggName;
+                msg.emdName = emdName;
 
-                $('#emdList').html(content);
+                // 핸들바 템플릿 컴파일
+                updateSelectEmd(msg);
+                updateSelectNav(msg);
+                updateSelectedElement("#districtSelectSggDHTML li", _this);
 
-                $("#sggList li").removeClass("on");
-                $(_this).addClass('on');
-
-                updateViewDistrictName();
             } else {
                 alert(JS_MESSAGE[msg.errorCode]);
                 console.log("---- " + msg.message);
@@ -259,7 +239,14 @@ function changeSgg(_this, _sdoCode, _sggCode) {
     });
 }
 
-// 읍면동을 선택
+/**
+ * 읍면동을 선택
+ * @param _this
+ * @param _sdoCode
+ * @param _sggCode
+ * @param _emdCode
+ * @returns {boolean}
+ */
 function changeEmd(_this, _sdoCode, _sggCode, _emdCode) {
     sdoCode = _sdoCode;
     sggCode = _sggCode;
@@ -269,35 +256,64 @@ function changeEmd(_this, _sdoCode, _sggCode, _emdCode) {
     if (!emdCode) {
         emdCode = "";
         districtMapType = 2;
-        $("#emdList li").removeClass("on");
-        $(_this).addClass('on');
-        $('#districtNav span').removeClass("on");
-        $('#sggName').addClass("on")
-        $('#sdoName').html(sdoName);
-        $('#sggName').html(sggName);
-        $('#emdName').html("읍면동");
+        var msg = {
+            sdoCode : sdoCode,
+            sggCode : sggCode,
+            emdCode : emdCode,
+            isSelectedSgg : true,
+            sdoName : sdoName,
+            sggName : sggName,
+            emdName : ""
+        };
+        updateSelectNav(msg);
+        updateSelectedElement("#districtSelectEmdDHTML li", _this);
         return false;
     }
 
     emdName = $(_this).text();
+    var msg = {
+        sdoCode : sdoCode,
+        sggCode : sggCode,
+        emdCode : emdCode,
+        isSelectedEmd : true,
+        sdoName : sdoName,
+        sggName : sggName,
+        emdName : emdName
+    };
+    updateSelectNav(msg);
+    updateSelectedElement("#districtSelectEmdDHTML li", _this);
+}
 
-    $('#districtNav span').removeClass("on");
-    $('#emdName').addClass("on")
-    $('#sdoName').html(sdoName ? sdoName : "시도");
-    $('#sggName').html(sggName ? sggName : "시군구");
-    $('#emdName').html(emdName ? emdName : "읍면동");
+function updateSelectSdo(msg) {
+    var templateSdo = Handlebars.compile($("#districtSelectSdoSource").html());
+    $("#districtSelectSdoDHTML").html("").append(templateSdo(msg));
+}
 
-    $("#emdList li").removeClass("on");
+function updateSelectSgg(msg) {
+    var templateSgg = Handlebars.compile($("#districtSelectSggSource").html());
+    $("#districtSelectSggDHTML").html("").append(templateSgg(msg));
+}
+
+function updateSelectEmd(msg) {
+    var templateEmd = Handlebars.compile($("#districtSelectEmdSource").html());
+    $("#districtSelectEmdDHTML").html("").append(templateEmd(msg));
+}
+
+function updateSelectNav(msg) {
+    var templateNav = Handlebars.compile($("#districtSelectNavSource").html());
+    $("#districtSelectNavDHTML").html("").append(templateNav(msg));
+}
+
+function updateSelectedElement(_parent, _this) {
+    $(_parent).removeClass("on");
     $(_this).addClass('on');
-
-    updateViewDistrictName();
 }
 
 $("#districtFlyButton").click(function () {
     var name = [sdoName, sggName, emdName].join(" ").trim();
     district.drawDistrict(name, sdoCode, sggCode, emdCode, bjcdLen);
-    //getCentroid(name, sdoCode, sggCode, emdCode);
     getEnvelope(name, sdoCode, sggCode, emdCode, bjcdLen);
+    //getCentroid(name, sdoCode, sggCode, emdCode);
 });
 
 $("#districtCancelButton").click(function () {
@@ -308,12 +324,70 @@ $("#districtCloseButton").click(function () {
     $("#districtSelect").click();
 });
 
+function getEnvelope(name, sdoCode, sggCode, emdCode, bjcdLen) {
+    var layerType = districtMapType;
+
+    var sdoCodeStr = sdoCode.toString();
+    var sggCodeStr = sggCode.toString();
+    var emdCodeStr = emdCode.toString();
+
+    // 시도(2) + 시군구(3) + 읍면동(3) + 리(2)
+    var bjcd;
+    if (bjcdLen == 10) {
+        bjcd = sdoCodeStr.padStart(2, '0')
+            + sggCodeStr.padStart(3, '0')
+            + emdCodeStr.padStart(3, '0') + '00';
+    } else {
+        bjcd = sdoCodeStr + sggCodeStr + emdCodeStr;
+    }
+
+    var info = "layerType=" + layerType + "&name=" + name + "&bjcd=" + bjcd;
+    $.ajax({
+        url: "../searchmap/envelope",
+        type: "GET",
+        data: info,
+        dataType: "json",
+        success: function (msg) {
+            if (msg.statusCode <= 200) {
+
+                var pointArray = [];
+                var minX = msg.minPoint[0];
+                var minY = msg.minPoint[1];
+                var maxX = msg.maxPoint[0];
+                var maxY = msg.maxPoint[1];
+
+                pointArray[0] = Mago3D.ManagerUtils.geographicCoordToWorldPoint(minX, minY, 0);
+                pointArray[1] = Mago3D.ManagerUtils.geographicCoordToWorldPoint(maxX, maxY, 0);
+
+                MAGO3D_INSTANCE.getMagoManager().flyToBox(pointArray);
+
+            } else {
+                alert(JS_MESSAGE[msg.errorCode]);
+                console.log("---- " + msg.message);
+            }
+        },
+        error: function (request, status, error) {
+            //alert(JS_MESSAGE["ajax.error.message"]);
+            console.log("code : " + request.status + "\n message : " + request.responseText + "\n error : " + error);
+        }
+    });
+}
+
 function getCentroid(name, sdoCode, sggCode, emdCode, bjcdLen) {
     var layerType = districtMapType;
-    if(bjcdLen==10){
-    	var bjcd = sdoCode.toString().padStart(2, '0') + sggCode.toString().padStart(3, '0') + emdCode.toString().padStart(3, '0') + '00';
-    }else{
-    	bjcd = sdoCode.toString() + sggCode.toString() + emdCode.toString();
+
+    var sdoCodeStr = sdoCode.toString();
+    var sggCodeStr = sggCode.toString();
+    var emdCodeStr = emdCode.toString();
+
+    // 시도(2) + 시군구(3) + 읍면동(3) + 리(2)
+    var bjcd;
+    if (bjcdLen == 10) {
+        bjcd = sdoCodeStr.padStart(2, '0')
+            + sggCodeStr.padStart(3, '0')
+            + emdCodeStr.padStart(3, '0') + '00';
+    } else {
+        bjcd = sdoCodeStr + sggCodeStr + emdCodeStr;
     }
     var time = 3;
 
@@ -344,58 +418,36 @@ function getCentroid(name, sdoCode, sggCode, emdCode, bjcdLen) {
     });
 }
 
-function getEnvelope(name, sdoCode, sggCode, emdCode, bjcdLen) {
-    var layerType = districtMapType;
-    var bjcd;
-    if (bjcdLen == 10) {
-        bjcd = sdoCode.toString().padStart(2, '0') + sggCode.toString().padStart(3, '0') + emdCode.toString().padStart(3, '0') + '00';
-    } else {
-        bjcd = sdoCode.toString() + sggCode.toString() + emdCode.toString();
-    }
-    
-    var time = 3;
-
-    var info = "layerType=" + layerType + "&name=" + name + "&bjcd=" + bjcd;
-    $.ajax({
-        url: "../searchmap/envelope",
-        type: "GET",
-        data: info,
-        dataType: "json",
-        success: function (msg) {
-            if (msg.statusCode <= 200) {
-
-                var pointArray = [];
-                var minX = msg.minPoint[0];
-                var minY = msg.minPoint[1];
-                var maxX = msg.maxPoint[0];
-                var maxY = msg.maxPoint[1];
-
-                pointArray[0] = Mago3D.ManagerUtils.geographicCoordToWorldPoint(minX, minY, 0);
-                pointArray[1] = Mago3D.ManagerUtils.geographicCoordToWorldPoint(maxX, maxY, 0);
-
-                MAGO3D_INSTANCE.getMagoManager().flyToBox(pointArray);
-
-                /*
-                var altitude = 50000;
-                if(layerType === 2) {
-                    altitude = 15000;
-                } else if(layerType === 3) {
-                    altitude = 1500;
-                }
-                gotoFly(msg.longitude, msg.latitude, altitude, time);
-                */
-            } else {
-                alert(JS_MESSAGE[msg.errorCode]);
-                console.log("---- " + msg.message);
-            }
-        },
-        error: function (request, status, error) {
-            //alert(JS_MESSAGE["ajax.error.message"]);
-            console.log("code : " + request.status + "\n message : " + request.responseText + "\n error : " + error);
-        }
-    });
-}
-
 function gotoFly(longitude, latitude, altitude, duration) {
     gotoFlyAPI(MAGO3D_INSTANCE, longitude, latitude, altitude, duration);
+}
+
+function updateViewDistrictName() {
+    // 시군구가 blank
+    if (sggName.trim() === "" || sdoName === sggName) {
+        // 읍면동이 blan 이거나 시도랑 같은 경우
+        if (emdName.trim() === "" || sdoName === emdName) {
+            $("#viewDistrictName").html([sdoName]);
+        } else {
+            $("#viewDistrictName").html([sdoName, emdName].join(" "));
+        }
+    } else {
+        // 시도랑 시군구랑 다를때
+        // 시도랑 읍면동이 같을때
+        if (sdoName === emdName) {
+            $("#viewDistrictName").html([sdoName, sggName].join(" "));
+        } else {
+            if (sggName === emdName) {
+                $("#viewDistrictName").html([sdoName, sggName].join(" "));
+            } else {
+                $("#viewDistrictName").html([sdoName, sggName, emdName].join(" "));
+            }
+        }
+
+        if (sggName === emdName) {
+            $("#viewDistrictName").html([sdoName, sggName].join(" "));
+        } else {
+            $("#viewDistrictName").html([sdoName, sggName, emdName].join(" "));
+        }
+    }
 }
