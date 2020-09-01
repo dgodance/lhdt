8509af527f9175347abea450efdc5a5e82f3394f
HyunSeongKil <aha1492@gmail.com>
2020-09-01 18:31:59 +0900

    add 지구가시화

--- lhdt-user/src/main/java/lhdt/controller/view/CityPlanController.java
+++ lhdt-user/src/main/java/lhdt/controller/view/CityPlanController.java
@@ -3,8 +3,21 @@
  */
 package lhdt.controller.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 지구 계획
@@ -45,6 +58,73 @@ public class CityPlanController {
 	public String cityPlanExc() {
 		
 		return P + "city-plan-exc"; 
+	}
+	
+	
+	/**
+	 * TODO 지구 목록
+	 * @return
+	 * @throws JsonProcessingException 
+	 */
+	@GetMapping("/lot-view/city-datas")
+	@ResponseBody
+	public ResponseEntity<Map<String,Object>> cityDatas() throws JsonProcessingException{		
+		
+		//
+		Map<String,Object> map = new HashMap<>();
+		
+		List<Map<String,Object>> dummyLists = new ArrayList<>();
+		//
+		Map<String,Object> m1 = new HashMap<>();
+		dummyLists.add(m1);
+		m1.put("cityNo", "c1");
+		m1.put("cityName", "하남 교산");
+		
+		//
+		Map<String,Object> m2 = new HashMap<>();
+		dummyLists.add(m2);
+		m2.put("cityNo", "c2");
+		m2.put("cityName", "과천 과천");
+		
+		//		
+		map.put("datas", dummyLists);
+		
+		//
+		return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+	}
+	
+	
+	
+	/**
+	 * TODO 지구의 레이어 목록
+	 * @return
+	 * @throws JsonProcessingException 
+	 */
+	@ResponseBody
+	@GetMapping("/lot-view/city-datas/{cityNo}/layer-datas")
+	public ResponseEntity<Map<String,Object>> layerDatas(@PathVariable("cityNo") String cityNo) throws JsonProcessingException{		
+		
+		//
+		Map<String,Object> map = new HashMap<>();
+		
+		List<Map<String,Object>> dummyLists = new ArrayList<>();
+		//
+		Map<String,Object> m1 = new HashMap<>();
+		dummyLists.add(m1);
+		m1.put("layerKey", "layerkey");
+		m1.put("layerName", cityNo + "'s layer명");
+		
+		//
+		Map<String,Object> m2 = new HashMap<>();
+		dummyLists.add(m2);
+		m2.put("layerKey", "sk_sdo");
+		m2.put("layerName", cityNo + "'s sk_sdo");
+		
+		//		
+		map.put("datas", dummyLists);
+		
+		//
+		return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
 	}
 	
 }
