76858deae8e7c19008beb69d5b1aa346c8450e5a
jdcheon <jdcheon@gaia3d.com>
2020-09-08 19:33:09 +0900

    insert 문 추가

--- doc/database/platform/dml/insert.sql
+++ doc/database/platform/dml/insert.sql
@@ -249,4 +249,32 @@ VALUES
     (11, '', '수원 당수2지구', 'admin', 1, 1, 2, 11, 0, true, true)
 ;
 
+INSERT INTO public.urban_group(
+	urban_group_id, urban_group_key, urban_group_name, user_id, ancestor, parent, depth, view_order, children, available, basic)
+VALUES
+    (1, 'a', '3기 신도시', 'admin', 1, 0, 1, 1, 10, true, true),
+    (2, 'b', '남양주 왕숙 신도시', 'admin', 1, 1, 2, 2, 0, true, true),
+    (3, 'c', '하남 교산 신도시', 'admin', 1, 1, 2, 3, 0, true, true),
+    (4, 'd', '인천 계양 신도시', 'admin', 1, 1, 2, 4, 0, true, true),
+    (5, 'e', '고양 창릉 신도시', 'admin', 1, 1, 2, 5, 0, true, true),
+    (6, 'f', '부천 대장 신도시', 'admin', 1, 1, 2, 6, 0, true, true),
+    (7, 'g', '과천 과천 지구', 'admin', 1, 1, 2, 7, 0, true, true),
+    (8, 'h', '안산 장상지구', 'admin', 1, 1, 2, 8, 0, true, true),
+    (9, 'i', '용인 구성역', 'admin', 1, 1, 2, 9, 0, true, true),
+    (10, 'j', '안산 신길2지구', 'admin', 1, 1, 2, 10, 0, true, true),
+    (11, 'k', '수원 당수2지구', 'admin', 1, 1, 2, 11, 0, true, true)
+;
+
+-- 건축한계선
+-- 구역계
+-- 벽면한계선
+-- 상층부관리구간
+-- 상층부관리구간
+-- 중저층배치구간
+-- 중저층배치권장구간
+-- 커뮤니티회랑
+-- 탑상형배치구간
+-- 탑상형배치권장구간
+-- 획지
+
 commit;
