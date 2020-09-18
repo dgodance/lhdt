c819b4e8b6a2bcc7fc82239d52a250e4dfd08174
HyunSeongKil <aha1492@gmail.com>
2020-09-18 13:54:10 +0900

    add

---
+++ lhdt-user/src/main/java/lhdt/domain/landscape/LandScapeDiffParam.java
@@ -0,0 +1,19 @@
+package lhdt.domain.landscape;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LandScapeDiffParam {
+    private Long id;
+    private Long landScapeDiffGroupId;
+    private String captureCameraState;
+    private String landscapeName;
+    private MultipartFile image;
+}
+
