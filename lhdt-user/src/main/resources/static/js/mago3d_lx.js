d6e4cce788469fce5040a30396d95d636d76cdde
kwunjaehyun <shinekill3@gmail.com>
2020-09-21 23:09:28 +0900

    이거저거

--- lhdt-user/src/main/resources/static/js/mago3d_lx.js
+++ lhdt-user/src/main/resources/static/js/mago3d_lx.js
@@ -73,3 +73,55 @@ Mago3D.ManagerUtils.getCoordinateFromWKT = function(wkt, type) {
 		}
 	}
 }
+
+Mago3D.MagoManager.prototype.validTerrainHeight = function() {
+	var allVisible = this.frustumVolumeControl.getAllVisiblesObject();
+	var nodeMap = allVisible.nodeMap;
+	var nativeMap = allVisible.nativeMap;
+	var that = this;
+	for(var k in nodeMap) {
+		if(nodeMap.hasOwnProperty(k)) {
+			var node = nodeMap[k];
+			var data = node.data;
+			if(!node.bboxAbsoluteCenterPos || data.valid) continue;
+
+			new Mago3D.Promise(function(resolve) {
+				node.data.valid = true;
+				resolve({mm:that,n:node});
+			}).then(function(obj){
+				var n = obj.n;
+				var bbox = n.getBBox();
+				var gg = Mago3D.ManagerUtils.pointToGeographicCoord(n.bboxAbsoluteCenterPos);
+				var viewer = MAGO3D_INSTANCE.getViewer();
+				var promise = Cesium.sampleTerrain(viewer.terrainProvider, 17, [Cesium.Cartographic.fromDegrees(gg.longitude, gg.latitude)]);
+				promise.then(function(t){
+					var h = t[0].height;
+					var cp = n.getCurrentGeoLocationData().geographicCoord;
+					n.changeLocationAndRotation(cp.latitude, cp.longitude, h - bbox.minZ, 0,0,0, obj.mm);
+				});
+			});
+		}
+	}
+
+	for(var k in nativeMap) {
+		if(nativeMap.hasOwnProperty(k)) {
+			var model = nativeMap[k];
+			if(model.valid) continue;
+			
+			new Mago3D.Promise(function(resolve) {
+				model.valid = true;
+				resolve({m:model});
+			}).then(function(obj){
+				var m = obj.m;
+				var geoLocData = m.getCurrentGeoLocationData();
+				var geoCoord = geoLocData.geographicCoord;
+				var viewer = MAGO3D_INSTANCE.getViewer();
+				var promise = Cesium.sampleTerrain(viewer.terrainProvider, 17, [Cesium.Cartographic.fromDegrees(geoCoord.longitude, geoCoord.latitude)]);
+				promise.then(function(t){
+					m.setTerrainHeight(t[0].height);
+				});
+			});
+		}
+	}
+	
+}
