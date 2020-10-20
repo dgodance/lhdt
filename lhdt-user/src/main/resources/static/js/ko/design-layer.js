6857847eaecad10327bdd3e6a52637991b5684f4
kwunjaehyun <shinekill3@gmail.com>
2020-10-20 09:53:29 +0900

    라벨변경

--- lhdt-user/src/main/resources/static/js/ko/design-layer.js
+++ lhdt-user/src/main/resources/static/js/ko/design-layer.js
@@ -2099,10 +2099,10 @@ DesignLayerObj.prototype.extrusionModelWMSLabelToggle = function(model, isShow){
           							label = _defaultLabelOption('주거ㆍ생활 컴플렉스존');
           							label.backgroundColor = Cesium.Color.fromCssColorString('#f1b531');
           						} else if( designLayerName === '도심형마켓존') {
-          							label = _defaultLabelOption('도심형 마켓 Zone');
+          							label = _defaultLabelOption('도심형 마켓 존');
           							label.backgroundColor = Cesium.Color.fromCssColorString('#f0575a');
           						} else if(designLayerName === '창의혁신클러스터존') {
-          							label = _defaultLabelOption('창의혁신 클러스터 Zone');
+          							label = _defaultLabelOption('창의혁신ㆍ산업 클러스터 존');
           							label.backgroundColor = Cesium.Color.fromCssColorString('#8c5a99');
           							label.pixelOffset = new Cesium.Cartesian2(120,0);
           						}
